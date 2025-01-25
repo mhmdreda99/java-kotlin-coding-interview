a174d81fc8a7b6d4b1a56814f5ca2406cc0d5c4f
Mohamed Reda <moreda491999@gmail.com>
2025-01-25 15:43:47 +0200

    #added more game test cases and  edited the code doc.

--- src/test/kotlin/com/example/rps/application/game/gameTest.kt
+++ src/test/kotlin/com/example/rps/application/game/gameTest.kt
@@ -7,11 +7,16 @@ import org.junit.jupiter.api.Assertions.*
 import io.mockk.every
 import io.mockk.mockk
 
+
 class GameTest {
     @Test
     fun `test game result totals equal rounds played`() {
-        val game = Game(mockk(), mockk())
-        every { game.play(100) } returns GameResult(31, 37, 32)
+        val playerA = mockk<Player>()
+        val playerB = mockk<Player>()
+        every { playerA.makeMove() } returns Move.ROCK
+        every { playerB.makeMove() } returns Move.PAPER
+
+        val game = Game(playerA, playerB)
         val result = game.play(100)
         assertEquals(100, result.playerAWins + result.playerBWins + result.draws)
     }
@@ -26,5 +31,41 @@ class GameTest {
         val game = Game(playerA, playerB)
         val result = game.play(1)
         assertEquals(GameResult(0, 0, 1), result)
+    }
+    // More test cases
+    @Test
+    fun `test paper beats rock`() {
+        val playerA = mockk<Player>()
+        val playerB = mockk<Player>()
+        every { playerA.makeMove() } returns Move.PAPER
+        every { playerB.makeMove() } returns Move.ROCK
+
+        val game = Game(playerA, playerB)
+        val result = game.play(1)
+        assertEquals(GameResult(1, 0, 0), result)
+    }
+    // Add for Rock VS Scissors
+    @Test
+    fun `test rock beats scissors`() {
+        val playerA = mockk<Player>()
+        val playerB = mockk<Player>()
+        every { playerA.makeMove() } returns Move.ROCK
+        every { playerB.makeMove() } returns Move.SCISSORS
+
+        val game = Game(playerA, playerB)
+        val result = game.play(1)
+        assertEquals(GameResult(1, 0, 0), result)
+    }
+    // Add for Scissors VS Paper
+    @Test
+    fun `test scissors beats paper`() {
+        val playerA = mockk<Player>()
+        val playerB = mockk<Player>()
+        every { playerA.makeMove() } returns Move.SCISSORS
+        every { playerB.makeMove() } returns Move.PAPER
+
+        val game = Game(playerA, playerB)
+        val result = game.play(1)
+        assertEquals(GameResult(1, 0, 0), result)
     }
 }
